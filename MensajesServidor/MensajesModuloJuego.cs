b9573c1681033013edfaa9725f60337ceaa4e13f
Igniel <walthervallejo2018@gmail.com>
2025-06-08 22:31:54 +0200

    4°commit:  logica para enviar mensajes al servidor modulo juego.

---
+++ MensajesServidor/MensajesModuloJuego.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MensajesServidor;
+
+public enum EnumMensajeJuego
+{
+    MandarPersonaje,
+    MandarEstado,
+    MandarHabilidad
+}
+
+public class MensajesModuloJuego
+{
+    public EnumMensajeJuego TipoMensaje { get; set; }
+    public string Valor { get; set; }
+}
